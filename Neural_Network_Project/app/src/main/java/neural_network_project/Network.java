715f50eb9dc82eb4a0dd8a174629047e2f7c7356
VincentNgo12 <121452382+VincentNgo12@users.noreply.github.com>
2023-08-14 23:23:40 +0000

    Backpropagation method

--- Neural_Network_Project/app/src/main/java/neural_network_project/Network.java
+++ Neural_Network_Project/app/src/main/java/neural_network_project/Network.java
@@ -38,14 +38,67 @@ public class Network{
 
 
     public INDArray feedforward(INDArray a){
-        for(int i = 0; i < this.weights.size(); i++){
+        for(int i = 0; i < this.biases.size(); i++){
             INDArray weightsMatrix = this.weights.get(i);
             INDArray biasesMatrix = this.biases.get(i);
-            a = weightsMatrix.mmul(a).add(biasesMatrix);
+            a = Nd4j.add(weightsMatrix.mmul(a),biasesMatrix);
             a = Transforms.sigmoid(a);
         }
 
         return a;
     }
 
+
+    public List<List<INDArray>> backpropagation(INDArray input, INDArray desiredOutput){
+        List<INDArray> gradient_biases = new ArrayList<>();
+        List<INDArray> gradient_weights = new ArrayList<>();
+        
+        for(INDArray weight:this.weights){
+            gradient_weights.add(Nd4j.zerosLike(weight));
+        }
+        for(INDArray bias:this.biases){
+            gradient_weights.add(Nd4j.zerosLike(bias));
+        }
+
+        INDArray activation = input;
+        List<INDArray> activations = new ArrayList<>();
+        activations.add(input);
+        List<INDArray> z_vectors = new ArrayList<>();
+
+        for(int i=0; i<this.biases.size(); i++){
+            INDArray weightsMatrix = this.weights.get(i);
+            INDArray biasesMatrix = this.biases.get(i);
+            INDArray z = Nd4j.add(weightsMatrix.mmul(activation),biasesMatrix);
+            z_vectors.add(z);
+            activation = Transforms.sigmoid(z);
+            activations.add(activation);
+        }
+
+        INDArray last_activation_layer = activations.get(activations.size()-1);
+        INDArray last_z_vector_layer = z_vectors.get(z_vectors.size()-1);
+        // Backward pass
+        INDArray delta_vector = this.cost_derivative(last_activation_layer, desiredOutput).mul(Transforms.sigmoidDerivative(last_z_vector_layer));
+        gradient_biases.set(gradient_biases.size()-1,delta_vector);
+        gradient_weights.set(gradient_weights.size()-1,delta_vector.mmul(activations.get(activations.size()-2).transpose()));
+
+
+        for(int layer=this.num_layers-2; layer > 0; layer--){
+            INDArray z = z_vectors.get(layer);
+            INDArray sigmoid_prime = Transforms.sigmoidDerivative(z);
+            delta_vector = this.weights.get(layer+1).transpose().mmul(delta_vector).mul(sigmoid_prime);
+
+            gradient_biases.set(layer, delta_vector);
+            gradient_weights.set(layer, delta_vector.mmul(activations.get(layer-1).transpose()));
+        }
+
+        List<List<INDArray>> gradients = new ArrayList<>();
+        gradients.add(gradient_biases);
+        gradients.add(gradient_weights);
+        return gradients;
+    }
+
+
+    public INDArray cost_derivative(INDArray output_activations, INDArray desiredOutput){
+        return Nd4j.sub(output_activations,desiredOutput);
+    }
 }
